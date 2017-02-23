e1184af5ae9266ec4a430c9c36b038d5e3e3953b
Turó Krisztián <scientful@gmail.com>
2017-02-23 09:19:00 +0100

    Update kontener.sh

--- kontener.sh
+++ kontener.sh
@@ -2,5 +2,5 @@
 lxc init ubuntu:16.04 krisztian
 lxc start krisztian
 sleep 15
-lxc exec krisztian --apt update -y
-lxc exec krisztian --apt upgrade -y
+lxc exec krisztian -- apt update -y
+lxc exec krisztian -- apt upgrade -y
